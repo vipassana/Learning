7656167da2a2612edb201a2f3d3fb07880b65398
vipassana <vipassana.me@gmail.com>
2016-05-25 14:50:23 -0400

    attempting suprocess creation

---
+++ Python/subprocess.py
@@ -0,0 +1,13 @@
+'''
+Code to spawn a subprocess that monitors stdin while parent does other things
+'''
+import subprocess
+import time
+
+if __name__ == '__main__':
+    i=0
+    ip_tracker = subprocess
+    while True:
+        print i
+        i += 1
+        time.sleep(0.5)
